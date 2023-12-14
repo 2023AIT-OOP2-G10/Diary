5c7a20705357ef399f58e2180d8e13e123e87753
ucbssnj <k22010kk@aitech.ac.jp>
2023-12-15 01:58:02 +0900

    diary修正3

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from diaries.KoukiDiary import DiaryKouki
 from diaries.satosomaDiary import Diarysatou
 from diaries.furutaDiary import Diaryfuruta
 from diaries.DiaryIchikawa import DiaryIchikawa
-
 # ↓のリストには、メンバーの各日記が格納されます。 
 diaries = [DiarySample(),
            DiaryKouki(),
            Diarysatou(),
            Diaryfuruta(),
            DiaryIchikawa(),
-]
-
+           ]
 for d in diaries:
       print("---------------------------------")
       print(d.get_date())

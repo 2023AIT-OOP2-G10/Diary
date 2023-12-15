823b0d3147c26adc4a8721523adc0191fbaa011c
shion-oO <k22070kk@aitech.ac.jp>
2023-12-15 18:50:17 +0900

    shion日記追加しました

---
+++ diaries/shionDiary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class Diaryshion(AbstractDiary):
+      def get_date(self):
+          return "2021-12-15"
+      def get_summary(self):
+        return "しんどい1日だった"
+      def get_author(self):
+          return "菅間志穏"
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from diaries.DiarySample import DiarySample
 from diaries.KoukiDiary import DiaryKouki
 from diaries.satosomaDiary import Diarysatou
 from diaries.furutaDiary import Diaryfuruta
+from diaries.shionDiary import Diaryshion
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            DiaryKouki(),
            Diarysatou(),
            Diaryfuruta(),
+           Diaryshion(),
            ]
 for d in diaries:
     print("---------------------------------")

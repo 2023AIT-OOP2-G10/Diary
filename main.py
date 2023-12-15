fb9942f853c390b5d85c638bab611972ed7f83d8
ucbssnj <k22010kk@aitech.ac.jp>
2023-12-15 09:16:08 +0900

    日記作成(市川)

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from diaries.DiarySample import DiarySample
 from diaries.KoukiDiary import DiaryKouki
 from diaries.satosomaDiary import Diarysatou
 from diaries.furutaDiary import Diaryfuruta
+from diaries.IchikawaDiary import Diaryichikawa
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            DiaryKouki(),
            Diarysatou(),
            Diaryfuruta(),
+           Diaryichikawa(),
            ]
 for d in diaries:
     print("---------------------------------")
